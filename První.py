d966e23663ef6ecdf3b9d49accada57e161ed15a
Yesseruser <matej.petricek@gmail.com>
2021-02-01 18:25:25 +0100

    Added a circle

--- První.py
+++ První.py
@@ -1,10 +1,12 @@
 import pygame
+
 pygame.init()
 
 velikost: tuple[int, int] = (800, 600)
 screen = pygame.display.set_mode(velikost)
 
 pygame.draw.rect(screen, 'white', (100, 100, 50, 50))
+pygame.draw.circle(screen, "green", (200, 150), 50)
 pygame.display.flip()
 
 run = True
