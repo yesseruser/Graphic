da2b8c64634171c6b00e4c2b38b85ab7ca952f83
yesseruser <67008763+yesseruser@users.noreply.github.com>
2021-02-15 17:54:10 +0100

    Renamed some files

--- Testing.py
+++ Testing.py
@@ -3,8 +3,8 @@ import pygame
 
 pygame.init()
 
-velikost: tuple[int, int] = (800, 600)
-screen = pygame.display.set_mode(velikost)
+size: tuple[int, int] = (800, 600)
+screen = pygame.display.set_mode(size)
 
 pygame.draw.rect(screen, 'white', (100, 100, 50, 50))
 pygame.draw.circle(screen, "green", (200, 150), 50)
